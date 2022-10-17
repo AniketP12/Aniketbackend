3d29e06d9ddde441cd98a3846381251a0dac345f
AniketP12 <aniketphatak4512@gmail.com>
2022-10-17 22:43:18 +0530

    assignment completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -39,5 +39,80 @@ router.get('/student-details/:name', function(req, res){
     console.log('Name of the student is ', studentName)
     res.send('Dummy response')
 })
+//problem no 1
+router.get('/movies',function(req,res){
+    let movies=['rang de basanti','the shining','lord of the rings','batman begins']
+    console.log("the list of movies",movies)
+    res.send('list of movies'+movies)
+})
 
-module.exports = router;
+//problem no 2
+router.get('/movies/:indexnumber',function(req,res){
+const movie$arr = ["Rang de basanti", "the shining",'lord of the rings','batman begins']
+const index=req.params.indexnumber
+console.group('print movie using index ==>',movie$arr)
+res.send('movie name by index :'+ movie$arr[index])
+
+})
+//problem no 3
+router.get('/movies3/:indexNumber',function(req,res){
+    const movies3=["Rang de basanti", "the shining",'lord of the rings','batman begins']
+    const index2=req.params.indexNumber
+    if(index2 >=movies3.length){
+        res.send("enter valid index no")
+    }
+    else{
+        res.send(movies3[index2])
+    }
+})
+
+//problem no 4
+
+router.get('/film1',function(req,res){
+    console.log("films are",req.params)
+   let film1 =[ {
+    "id": 1,
+    "name": 'The Shining'
+   }, {
+    "id": 2,
+    "name": "Incendies"
+   }, {
+    "id": 3,
+    "name": "Rang de Basanti"
+   }, {
+    "id": 4,
+    "name": "Finding Nemo"
+   }]
+   
+    res.send(film1)
+   // res.send('list of movies'+film1)
+
+
+   
+})
+ module.exports = router;
+
+ //problem no 5
+
+ router.get('/films/:filmid', function (req, res) {                               
+    let obj2 = [{
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name":"Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+      let q=req.params.filmid
+      if(q > obj2.length  ||  q==0){                                                  
+      res.send("No movie exists with this id")
+      }else{
+     let q=req.params.filmid-1 
+      res.send(obj2[q]) 
+    }
+});
